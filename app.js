61cc6518f19613fd93f9b3668069e706c6b3be67
cartao078 <documentosctc01@gmail.com>
2026-02-02 13:07:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_URL = 'https://script.google.com/macros/s/AKfycbxsjfbI6Z33q4wr69Tb-Fw66WtfpfaOhspYZ12dqqyOGrPd1PV5ahe9FCzvS0k1wpx7Rg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyFrAeP48Dcq7p1krCZSWNE0AoXB7krjo6OrW5_sJNu9Pc-raY-XWnVy6McX22i6VjzPg/exec';
 
 // State
 let currentDashboard = 'documentacao';
